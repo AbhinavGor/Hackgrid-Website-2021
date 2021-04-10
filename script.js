0d22fc0d3cd0cfd4114355d5153c6ddbe0160e7c
Divesh Arora <diveshngp@gmail.com>
2021-04-10 19:39:59 +0530

    updated

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -13,5 +18,29 @@ function scrollFunction() {
 function topFunction() {
   //document.body.scrollTop = 0;
   //document.documentElement.scrollTop = 0;
-  window.scrollTo({top: 0, behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 6 + 0.5
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
