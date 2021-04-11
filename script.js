0a876fc761c0b01690ed4bddebf406431baefe89
DevSwaika99 <66863974+DevSwaika99@users.noreply.github.com>
2021-04-11 14:24:37 +0530

    Landing Page Changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.onload=function(){
 mybutton = document.getElementById("myBtn");
 
 window.onscroll = function () {
@@ -13,12 +14,6 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
-
-function topFunction() {
-  //document.body.scrollTop = 0;
-  //document.documentElement.scrollTop = 0;
-  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 const navSlide = () => {
@@ -44,3 +39,32 @@ const navSlide = () => {
 };
 
 navSlide();
+
+var countDownDate = new Date("Apr 13, 2021 15:00:30").getTime();
+
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+  
+var timeleft = countDownDate - now;
+      
+  
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  if (days<10) {document.getElementById("days").innerHTML = `0${days}`}
+  else {document.getElementById("days").innerHTML = `${days}`}
+
+  if (hours<10) {document.getElementById("hours").innerHTML = `0${hours}`}
+  else {document.getElementById("hours").innerHTML = `${hours}`}
+  
+  if (minutes<10){document.getElementById("mins").innerHTML = `0${minutes}`}
+  else {document.getElementById("mins").innerHTML = `${minutes}`}
+
+  if (seconds<10){document.getElementById("secs").innerHTML = `0${seconds}`}
+  else {document.getElementById("secs").innerHTML = `${seconds}`}
+
+}, 1000);
+}
